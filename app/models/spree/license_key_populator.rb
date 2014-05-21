69c17b7476da3dbc593c19acc0df50a23607d945
Chris Salzberg <csalzberg@degica.com>
2014-05-21 15:21:09 +0900

    Replace update_all with update_attributes on each license key.
    In the end it's too problematic to use an update_all here, since we need
    callbacks to be run, etc. There are only ever a few license keys anyway
    so this should not result in any serious performance issues.
    
    Change-Id: Ib9e58103acc36cb41b332a9edadaf766a5fa9307

--- app/models/spree/license_key_populator.rb
+++ app/models/spree/license_key_populator.rb
@@ -25,7 +25,7 @@ module Spree
     private
 
     def self.assign_keys!(keys, inventory_unit)
-      keys.update_all(inventory_unit_id: inventory_unit.id, updated_at: DateTime.now)
+      keys.each { |key| key.update_attributes!(inventory_unit_id: inventory_unit.id) }
     end
   end
 end
