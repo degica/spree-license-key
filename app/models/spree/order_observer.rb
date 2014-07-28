fa3e389c0daf33a924042889d1eb7ac04f4106e7
Chris Salzberg <chrissalzberg@gmail.com>
2014-07-28 09:06:47 +0900

    Log after_transition callback in Spree::OrderObserver.
    This callback method looks like it serves no purpose. Just to be sure,
    we'll log calls to it and see if it is being called from anywhere.

--- app/models/spree/order_observer.rb
+++ app/models/spree/order_observer.rb
@@ -1,6 +1,7 @@
 class Spree::OrderObserver < ActiveRecord::Observer
   def after_transition(order, transition)
     if transition.to == "complete" && order.paid?
+      Rails.logger.info "Spree::OrderObserver called in SpreeLicenseKey"
       order.after_finalize!
     end
   end
